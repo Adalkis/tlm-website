c21bf4237bec49bad4e0fdd93af032219e276648
Adalkis <adalkis2000@gmail.com>
2023-09-19 22:23:24 -0400

    Package json was updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from "./pages/HomePage"
 function App() {
 
   return (
-    <BrowserRouter basename="/tlm-website">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
       </Routes>
